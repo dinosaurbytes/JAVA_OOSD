c992166fb6a39cbc206e66d41bcf591dcd8c2555
Brian Liang <brianliang@dinosaurbytes.com>
2018-08-27 14:52:09 -0600

    Day 1 of Java Class

--- Module1/HelloWorld/src/HelloWorld.java
+++ Module1/HelloWorld/src/HelloWorld.java
@@ -3,14 +3,19 @@ public class HelloWorld
     public static void main (String [] args)
     {
         int x =5;
+        int y = 10;
         boolean b = true;
         char c = 'c';
         String s = "I'm a string object";
+
 
         System.out.println("Hello World!");
         System.out.println(x);
         System.out.println(b);
         System.out.println(c);
         System.out.println(s);
+        System.out.println("ssssssss".length());  //in quotes, it auto creates a string object, without having to declare it.
+
+   
     }
 }
